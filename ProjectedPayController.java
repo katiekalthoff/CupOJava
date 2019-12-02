9901a18468abf80b8d909e6b7cb51efa2c7ae070
katiekalthoff <55768526+katiekalthoff@users.noreply.github.com>
2019-12-02 13:33:36 -0600

    Create ProjectedPayController.java

---
+++ ProjectedPayController.java
@@ -0,0 +1,64 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ProjectedPayController {
+
+	
+	@FXML
+	private TextField inMon1;
+	@FXML
+	private TextField outMon1;
+	@FXML
+	private TextField inTue1;
+	@FXML
+	private TextField outTue1;
+	@FXML
+	private TextField inWed1;
+	@FXML
+	private TextField outWed1;
+	@FXML
+	private TextField inThur1;
+	@FXML
+	private TextField outThur1;
+	@FXML
+	private TextField inFri1;
+	@FXML
+	private TextField outFri1;
+	@FXML
+	private TextField inMon2;
+	@FXML
+	private TextField outMon2;
+	@FXML
+	private TextField inTue2;
+	@FXML
+	private TextField outTue2;
+	@FXML
+	private TextField inWed2;
+	@FXML
+	private TextField outWed2;
+	@FXML
+	private TextField inThur2;
+	@FXML
+	private TextField outThur2;
+	@FXML
+	private TextField inFri2;
+	@FXML
+	private TextField outFri2;
+	@FXML
+	private Label hoursWorked;
+    @FXML
+    private Label projectedPay;
+    @FXML
+    private Button returnToProfile;
+    @FXML
+    private Button calculatePay;
+    @FXML
+    private Button logOut;
+    
+    
+	
+}
